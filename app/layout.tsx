fb69fbaebdf2d73920e7447251b58fe7e894cd24
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-16 05:05:39 +0530

    added analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Providers from "@/lib/ReactQueryClientProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Analytics />
           <Providers>{children}</Providers>
           <Toaster position="top-right" richColors closeButton />
         </ThemeProvider>
