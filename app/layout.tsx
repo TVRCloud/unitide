93579339eba21d4e1c5db3189fffa08b13f29b59
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-16 21:15:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Analytics />
+
           <Providers>{children}</Providers>
           <Toaster position="top-right" richColors closeButton />
         </ThemeProvider>
