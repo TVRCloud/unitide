2ec9348532b21f375553d7c81f098725286e8e26
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-24 15:52:29 +0530

    Create page.tsx

---
+++ app/(MainLayout)/tasks/[id]/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrap from "@/components/layout-wrap";
+import ViewTaskMain from "@/components/tasks/ViewTaskMain";
+
+const ViewTask = () => {
+  return (
+    <LayoutWrap>
+      <ViewTaskMain />
+    </LayoutWrap>
+  );
+};
+
+export default ViewTask;
