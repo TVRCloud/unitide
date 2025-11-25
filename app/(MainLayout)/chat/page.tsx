28f168d640fa91cecd97370db5ba48fff4095174
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-25 12:51:46 +0530

    Create page.tsx

---
+++ app/(MainLayout)/chat/page.tsx
@@ -0,0 +1,12 @@
+import ChatMain from "@/components/chat/ChatMain";
+import LayoutWrap from "@/components/layout-wrap";
+
+const Chat = () => {
+  return (
+    <LayoutWrap>
+      <ChatMain />
+    </LayoutWrap>
+  );
+};
+
+export default Chat;
