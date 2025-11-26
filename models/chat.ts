ab0f7135c3bb661413f5fedfd4e0274aeb929870
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-26 13:55:49 +0530

    Update chat.ts

--- models/chat.ts
+++ models/chat.ts
@@ -2,12 +2,15 @@ import { Schema, model, models } from "mongoose";
 
 const ChatSchema = new Schema(
   {
-    title: { type: String },
+    title: String,
     type: { type: String, enum: ["GROUP", "DIRECT"], default: "DIRECT" },
     members: [{ type: Schema.Types.ObjectId, ref: "Users", required: true }],
     admins: [{ type: Schema.Types.ObjectId, ref: "Users" }],
     avatar: String,
     metadata: Schema.Types.Mixed,
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message" },
+    unreadCounts: { type: Map, of: Number, default: {} },
+    typing: [{ type: Schema.Types.ObjectId, ref: "Users" }],
     isPinned: { type: Boolean, default: false },
     isArchived: { type: Boolean, default: false },
   },
