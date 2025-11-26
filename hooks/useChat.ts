f711de19a4dea80c02b54691fcecd4efe27bd9ec
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-26 13:55:44 +0530

    Update useChat.ts

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,5 +1,16 @@
-import { createChat, fetchChats, fetchSingleChat } from "@/lib/api-client";
-import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
+import {
+  createChat,
+  fetchChatMessages,
+  fetchChats,
+  fetchSingleChat,
+  sendMessage,
+} from "@/lib/api-client";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
 export const useCreateChat = () => {
   return useMutation({
@@ -27,5 +38,29 @@ export const useViewChat = (id: string) => {
     enabled: !!id,
     queryKey: ["chat", id],
     queryFn: () => fetchSingleChat(id),
+  });
+};
+
+export const useViewChatMessages = (chatId: string) => {
+  return useInfiniteQuery({
+    queryKey: ["chat-message", chatId],
+
+    queryFn: ({ pageParam = 0 }) => fetchChatMessages(chatId, pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.length < 20 ? undefined : allPages.length * 20,
+  });
+};
+
+export const useSendMessage = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: sendMessage,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["chats"] });
+      queryClient.invalidateQueries({ queryKey: ["chat"] });
+    },
   });
 };
