8c75b9150a82ffdf2a4ef7310cf6f5dfe12fa45c
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-01 14:14:51 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -96,7 +96,7 @@ export function Header() {
                   className="relative h-8 w-8 rounded-full"
                 >
                   <Avatar className="h-8 w-8">
-                    <AvatarImage src={user?.image || ""} alt={user?.name} />
+                    <AvatarImage src={user?.avatar || ""} alt={user?.name} />
                     <AvatarFallback>
                       {user?.name ? user.name.charAt(0).toUpperCase() : "?"}
                     </AvatarFallback>
