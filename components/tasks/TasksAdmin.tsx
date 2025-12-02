2349f6f8736041e35a717b73e148ae03026c3b91
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-02 14:45:54 +0530

    Update TasksAdmin.tsx

--- components/tasks/TasksAdmin.tsx
+++ components/tasks/TasksAdmin.tsx
@@ -1,5 +1,28 @@
+"use client";
+import { useViewTaskStats } from "@/hooks/useTask";
+import { Button } from "../ui/button";
+import { HeaderSection } from "../ui/header-section";
+
 const TasksAdmin = () => {
-  return <div>TasksAdmin</div>;
+  const { data, isLoading } = useViewTaskStats();
+
+  if (isLoading) {
+    return <div>Loading Task Data...</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      <HeaderSection
+        title="Tasks"
+        subtitle="Overview of all tasks and team performance"
+        actions={
+          <div>
+            <Button variant="secondary">View all</Button>
+          </div>
+        }
+      />
+    </div>
+  );
 };
 
 export default TasksAdmin;
