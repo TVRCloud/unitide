025d1225a000d22b0baee30b3746fa697857afb5
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-19 16:46:01 +0530

    Update ChatWithUser.tsx

--- components/members/ChatWithUser.tsx
+++ components/members/ChatWithUser.tsx
@@ -1,38 +1,33 @@
 import { MessageSquarePlus } from "lucide-react";
 import { DropdownMenuItem } from "../ui/dropdown-menu";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
-import { useCreateChat } from "@/hooks/useChats";
 
 type Props = {
   userId: string;
   userName: string;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ChatWithUser = ({ userId, userName }: Props) => {
-  const createChat = useCreateChat();
-  const router = useRouter();
-
-  const onChatClick = () => {
-    createChat.mutate(
-      {
-        title: userName,
-        type: "DIRECT",
-        members: [userId],
-      },
-      {
-        onSuccess: () => {
-          router.push(`/chat?chatId=${userId}`);
-          toast.success("User created successfully");
-        },
-        onError: () => {
-          toast.error("Something went wrong");
-        },
-      }
-    );
-  };
+  // const onChatClick = () => {
+  //   createChat.mutate(
+  //     {
+  //       title: userName,
+  //       type: "DIRECT",
+  //       members: [userId],
+  //     },
+  //     {
+  //       onSuccess: () => {
+  //         router.push(`/chat?chatId=${userId}`);
+  //         toast.success("User created successfully");
+  //       },
+  //       onError: () => {
+  //         toast.error("Something went wrong");
+  //       },
+  //     }
+  //   );
+  // };
   return (
-    <DropdownMenuItem onClick={onChatClick}>
+    <DropdownMenuItem>
       <MessageSquarePlus className="mr-2 h-4 w-4" />
       Chat
     </DropdownMenuItem>
