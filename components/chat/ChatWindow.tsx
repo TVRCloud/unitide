d98635d67eee83bdf3c9afbea418979bf84d343a
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-22 13:33:19 +0530

    Update ChatWindow.tsx

--- components/chat/ChatWindow.tsx
+++ components/chat/ChatWindow.tsx
@@ -1,8 +1,12 @@
+import { useGetChatById } from "@/hooks/useChats";
+
 type Props = {
   chatId: string;
   onBack: () => void;
 };
 const ChatWindow = ({ chatId, onBack }: Props) => {
+  const chatDetails = useGetChatById(chatId);
+
   return <div>ChatWindow</div>;
 };
 
