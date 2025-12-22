fa4a722afe3f12616052d672ec31a466f344ba40
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-22 11:53:27 +0530

    Update useChats.ts

--- hooks/useChats.ts
+++ hooks/useChats.ts
@@ -1,5 +1,23 @@
-import { createPrivateChat } from "@/lib/api-client";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createPrivateChat, fetchChats } from "@/lib/api-client";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+
+export const useInfiniteChats = (search: string) => {
+  return useInfiniteQuery({
+    queryKey: ["all-chats", search],
+    queryFn: ({ pageParam = 0 }) =>
+      fetchChats({
+        skip: pageParam,
+        search,
+      }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.length < 20 ? undefined : allPages.length * 20,
+  });
+};
 
 export const useCreateChat = () => {
   const queryClient = useQueryClient();
