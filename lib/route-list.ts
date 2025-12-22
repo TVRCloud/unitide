5c7f10ade64d882a40465fbce5061e2f3819cbaf
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-22 12:30:16 +0530

    Update route-list.ts

--- lib/route-list.ts
+++ lib/route-list.ts
@@ -1,11 +1,23 @@
 export const protectedRoutes = [
+  "/chat",
   "/dashboard",
-  "/users",
-  "/files",
-  "/feedback",
-  "/settings",
   "/members",
-  "/logs",
+  "/members/[id]",
+  "/notifications",
+  "/notifications/[id]",
+  "/profile",
+  "/projects",
+  "/projects/[id]",
+  "/projects/active",
+  "/projects/completed",
+  "/settings/app",
+  "/settings/logs",
+  "/settings/session",
+  "/tasks",
+  "/tasks/[id]",
+  "/tasks/all",
+  "/teams",
+  "/teams/[id]",
 ];
 export const authRoutes = ["/login", "/register"];
 export const publicRoutes = ["/"];
