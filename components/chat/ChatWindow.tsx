d9456a64cda6caa3cab7c7a21f8a74a1dcfbe887
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-22 14:28:38 +0530

    Update ChatWindow.tsx

--- components/chat/ChatWindow.tsx
+++ components/chat/ChatWindow.tsx
@@ -1,21 +1,27 @@
-import { useGetChatById } from "@/hooks/useChats";
+import { useGetChatById, useGetMessagesByChatId } from "@/hooks/useChats";
 import { Button } from "../ui/button";
-import { ArrowLeft, MoreVertical, Phone, Video } from "lucide-react";
+import { MoreVertical, Phone, Video } from "lucide-react";
 import { SignedAvatar } from "../ui/signed-avatar";
 import { getChatAvatar, getChatName } from "@/utils/chats";
 import { useAuth } from "@/hooks/useUser";
+import { ScrollArea } from "../ui/scroll-area";
+import { useRef } from "react";
 
 type Props = {
   chatId: string;
 };
 const ChatWindow = ({ chatId }: Props) => {
   const { user } = useAuth();
+  const scrollRef = useRef<HTMLDivElement>(null);
+
   const { data: chat, isLoading } = useGetChatById(chatId);
+  const { data: messages } = useGetMessagesByChatId(chatId);
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="flex h-full flex-col">
+      {/* header */}
       <div className="flex items-center gap-3 border-b bg-background p-4">
         <SignedAvatar
           src={getChatAvatar(chat, user)}
@@ -44,6 +50,29 @@ const ChatWindow = ({ chatId }: Props) => {
           <MoreVertical className="h-5 w-5" />
         </Button>
       </div>
+
+      {/* Messages */}
+
+      <ScrollArea className="flex-1 p-4">
+        <div className="space-y-4">
+          {/* {messages.map((message) => (
+            <MessageBubble
+              key={message._id}
+              message={message}
+              isOwn={message.senderId._id === user?.id}
+            />
+          ))} */}
+          <div ref={scrollRef} />
+        </div>
+
+        {/* {messages.length === 0 && (
+          <div className="flex h-full items-center justify-center">
+            <p className="text-muted-foreground">
+              No messages yet. Start the conversation!
+            </p>
+          </div>
+        )} */}
+      </ScrollArea>
     </div>
   );
 };
