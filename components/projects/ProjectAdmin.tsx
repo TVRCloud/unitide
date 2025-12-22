1863fadb51ddd7e8ff5674c4c00764cb2d08b547
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-22 17:01:51 +0530

    Update ProjectAdmin.tsx

--- components/projects/ProjectAdmin.tsx
+++ components/projects/ProjectAdmin.tsx
@@ -1,4 +1,25 @@
-const ProjectAdmin = () => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Project } from "@/types/project";
+
+type Props = {
+  searchTerm: string;
+  setSearchTerm: (value: string) => void;
+  isLoading?: boolean;
+  filteredProjects: Project[];
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean;
+  ref: any;
+};
+
+const ProjectAdmin = ({
+  searchTerm,
+  setSearchTerm,
+  isLoading,
+  filteredProjects,
+  isFetchingNextPage,
+  hasNextPage,
+  ref,
+}: Props) => {
   return <div>ProjectAdmin</div>;
 };
 
