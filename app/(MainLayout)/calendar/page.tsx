02c4ee1a16dca73f95b9d2167b29a67f910c70ca
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-24 10:45:47 +0530

    Create page.tsx

---
+++ app/(MainLayout)/calendar/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrap from "@/components/layout-wrap";
+import CalenderMain from "@/components/calendar/CalenderMain";
+
+const Calender = () => {
+  return (
+    <LayoutWrap>
+      <CalenderMain />
+    </LayoutWrap>
+  );
+};
+
+export default Calender;
